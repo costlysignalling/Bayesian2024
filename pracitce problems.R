35c8427e29a5a37a9d85ce6f12bf373e922aecce
Petr Tureček <petr.turecek@natur.cuni.cz>
2023-12-06 11:14:31 +0100

    Update pracitce problems.R

--- pracitce problems.R
+++ pracitce problems.R
@@ -1,6 +1,6 @@
 #Exercise: Come up with an interesting problem/system with about 2-6 parameters, simulate it in the same way. If you cannot come up with anything, simulate one of these relationships (assigned during the workshop), use the "Maximum entropy inspired flowchart", "Data distribution illustrated with fish", or any other wisdom found on the handout.
 
-#Easy
+#Easy (no link functions)
 #Aliens1: dependent: number of teeth of an alien, independent: weight of an alien
 #Aliens2: dependent: weight of an alien, independent: number of teeth of an alien
 #Shamans: dependent: number of religious specialists, independent: group size
@@ -8,7 +8,7 @@
 #Beer and science: dependent: number of publications per year, independent: yearly beer consumption (liters)
 #Generosity: dependent: sustenance donated to a food bank (each participant receives 8 kg of dry rice at the beginning of the experiment) dependent: gender (introduce as many as you like) 
 
-#Medium
+#Medium (link functions or other specialities)
 #Butterflies: dependent: UV reflectance (as the proportion of wing that reflects UV), independent:  latitude and elevation.
 #Plants: dependent: plant height after 3 weeks, independent: 2 types of fertilizer and the amount of fertilizer vs non-fertilized control
 #1 of Big 5: dependent: Extraversion, independent: sex (male, female), hormonal contraception usage in females
@@ -16,7 +16,7 @@
 #Imprinting: dependent: eye color of the partner (same as parent / different from parent), independent: eye color of the parent (frequencies: black: 0.1, brown: 0.4, green: 0.2, blue: 0.3, note that frequencies in the pool of partners is expected to be the same)
 #Conformity: dependent: conformity (score based on yes/no questionnaire of 20 questions) independent: least effort distance of birthplace from the nearest origin of cereal agriculture 
 
-#Hard
+#Hard (crazy combinations of multiple problems)
 #Coats: dependent: How do you like the coat on a scale from 1 to 7, independent: color of the coat (8 different colors), time it took for the seamstress to make the coat, group of raters (fashion professional, layman, blind person)
 #Pragmatic altruism: dependent: Number of coins offered in the ultimatum game from the budget of 100 tokens (only discrete counts and there is an inflation above the middle that represents fair share), independent: testosterone level (nmol/litre), sex
 #Tools: dependent: number of distinct tool-types in archaeological record, dependent: island (4 different islands, each measured in three well-documented consecutive layers), presence of long-distance trade (assessed based on other signs from the archaeological record)
